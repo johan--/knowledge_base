4749280a5d900f80c92023c9385ae9fe6c37093b
CircleCI <ops@dobt.co>
2015-03-05 17:40:58 +0000

    Deploy (via Grunt)

--- js/main.js
+++ js/main.js
@@ -78,6 +78,10 @@
     });
     if ($(".search-results")[0]) {
       query = $.url().param('q');
+      if (!(query.length > 0)) {
+        $(".search-results-loading").remove();
+        return $(".no-query").show();
+      }
       $(".centersearch-input").val(query);
       $.getJSON(SEARCH_ENDPOINT, {
         q: query
